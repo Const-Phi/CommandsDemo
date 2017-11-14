ce4de59a739f57209f77fb23576d065a7907fec1
student <student@4525WS00>
2017-11-14 12:25:06 +0300

    Add project files.

---
+++ CustomCommands.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Input;
+
+namespace ResorcesDemo
+{
+    public static class CustomCommands
+    {
+        public static readonly RoutedUICommand Exit = new RoutedUICommand
+        (
+                text: "Выход",
+                name: "Exit",
+                ownerType: typeof(CustomCommands),
+                inputGestures: new InputGestureCollection()
+                {
+                    new KeyGesture(Key.F3, ModifierKeys.Alt)
+                }
+        );
+
+        //Define more commands here, just like the one above
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ResorcesDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CommandNew_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void CommandBinding_PreviewExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            var menuItem = e.Source as MenuItem;
+            if (menuItem == null)
+            {
+                MessageBox.Show("Команда отработала");
+                return;
+            }
+
+            MessageBox.Show($"Вы кликнули по пункту \"{menuItem.Header.ToString()}\"!");
+        }
+
+        private void CommandBinding_PreviewExecuted_1(object sender, ExecutedRoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
